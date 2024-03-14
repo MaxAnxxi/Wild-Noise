0557fe5c15240600cba06e4cc87525efb5a41523
fadili6 <diallofadili@yahoo.fr>
2024-03-14 15:34:43 +0100

    new_article

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-
+function showImage(./assets/images / hedgehog - 3703244_1280.jpg) {
+    hiddenImage = document.getElementById("#article>#description");
+    hiddenImage.style.display = 'block'; // Modifie le style CSS pour afficher l'image
+}
 
