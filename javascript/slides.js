b7ee9f56ee76730fda5ed5bf8ffb40aac75fb8ad
Loic legoff <loic95280@gmail.com>
2024-03-20 11:43:57 +0100

    update score

--- javascript/slides.js
+++ javascript/slides.js
@@ -10,7 +10,6 @@ const animals = [
         answer3: "Skunk",
         answer4: "Fox",
     },
-
 ];
 const animalDescription = document.getElementById("text-hedgehog");
 const listAnimal = animalDescription.getElementsByTagName("li");
@@ -18,40 +17,42 @@ const buttonQuizz = document.getElementsByClassName("button-quizz");
 const animalSound = document.getElementById("audio-hedgehog");
 const animalImg = document.getElementById("img-hedgehog");
 
-function createSlide(){
-    for(let i =0 ; i < animals.length;i++){
+function createSlide() {
+    for (let i = 0; i < animals.length; i++) {
         for (let j = 0; j < buttonQuizz.length; j++) {
             buttonQuizz[j].addEventListener('click', () => {
                 setTimeout(() => {
                     console.log("Yo Change sound")
                     animalSound.pause();
                     animalSound.currentTime = 0;
-                    animalSound.src=animals[i].sound;
-                    animalImg.src=animals[i].img;
-                    buttonQuizz[0].textContent=animals[i].answer1;
-                    buttonQuizz[1].textContent=animals[i].answer2;
-                    buttonQuizz[2].textContent=animals[i].answer3;
-                    buttonQuizz[3].textContent=animals[i].answer4;
+                    animalSound.src = animals[i].sound;
+                    animalImg.src = animals[i].img;
+                    buttonQuizz[0].textContent = animals[i].answer1;
+                    buttonQuizz[1].textContent = animals[i].answer2;
+                    buttonQuizz[2].textContent = animals[i].answer3;
+                    buttonQuizz[3].textContent = animals[i].answer4;
                     buttonQuizz[j].style.backgroundColor = "#D4EED3";
-                    listAnimal[0].textContent=animals[i].name;
-                    listAnimal[1].textContent=animals[i].family;
-                    listAnimal[2].textContent=animals[i].funfact;
-                    for(let k=0; k < buttonQuizz.length;k++){
-                        if(buttonQuizz[k].innerHTML === animals[i].name){
-                            buttonQuizz[k].setAttribute("data-rep","correct");
+                    listAnimal[0].textContent = animals[i].name;
+                    listAnimal[1].textContent = animals[i].family;
+                    listAnimal[2].textContent = animals[i].funfact;
+                    for (let k = 0; k < buttonQuizz.length; k++) {
+                        if (buttonQuizz[k].innerHTML === animals[i].name) {
+                            buttonQuizz[k].setAttribute("data-rep", "correct");
                             console.log("Good Answer");
                         }
-                        else{
+                        else {
                             buttonQuizz[k].removeAttribute("data-rep");
                             console.log("Bad Answer");
                         }
                     }
-                    }
-        , 3000);
-                
+                }
+                    , 3000);
+
             })
         }
-}
+    }
 }
 
 createSlide();
+
+export { animals };
