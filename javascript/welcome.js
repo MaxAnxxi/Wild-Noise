506b02b0af3a5b3ab9480e668f7a0cc4368dd605
Loic legoff <loic95280@gmail.com>
2024-03-21 16:32:50 +0100

    commentResult

--- javascript/welcome.js
+++ javascript/welcome.js
@@ -1,19 +1,19 @@
 //*------------------- playerName ---------------*//
+let balisePseudo = document.getElementById("pseudo");
+let resetButton = document.getElementById("resetButton");
+let baliseButton = document.getElementById("myButton");
+let dname = document.getElementById("name");
+let pseudoForm = document.getElementById("formrep")
+
+
+//*------------------- playerName ---------------*//
 function playerName() {
-    let balisePseudo = document.getElementById("pseudo");
-    let baliseButton = document.getElementById("myButton");
-    let dname = document.getElementById("name");
-    let pseudoForm = document.getElementById("formrep")
-    let resetButton = document.getElementById("resetButton");
 
     baliseButton.addEventListener("click", (event) => {
         event.preventDefault();
         dname.innerText = "Hello " + balisePseudo.value + "!";
         dname.style.fontSize = "15px";
         pseudoForm.style.display = "none";
-    });
-    resetButton.addEventListener("click", (event) => {
-        location.reload(event);
     });
 }
 playerName();
@@ -23,11 +23,16 @@ elt.maxLength = 10;
 
 //*------------------- score ---------------*//
 const reponses = [
-    document.getElementById("answer-hedgehog")
+    document.getElementById("answer-hedgehog"),
+    document.getElementById("answer-squirrel"),
+    document.getElementById("answer-hamster"),
+    document.getElementById("answer-beaver")
 ];
 
+
+let result = 0;
+
 function score() {
-    let result = 0;
     let scoreElement = document.getElementById("score");
 
     reponses.forEach((reponse) => {
@@ -35,8 +40,6 @@ function score() {
             if (reponse.dataset.rep === "correct") {
                 result++;
                 scoreElement.textContent = result;
-            } else {
-
             }
         });
     });
@@ -44,6 +47,7 @@ function score() {
 score();
 
 //*------------------- numberQuizz ---------------*//
+
 import { animals } from "./slides.js";
 console.log(animals);
 
@@ -51,8 +55,60 @@ const numberQz = [
     document.getElementById("numberQuiz")
 ];
 
-let numbertabl = animals.length;
+
+let numbertabl = animals.length + 1;
 console.log(numbertabl);
+numberQuiz.textContent = numbertabl;
 
-numberQuiz.textContent = numbertabl + 1;
 
+//*------------------- conclusion ---------------*//
+
+
+
+// const
+const elementHtml = [
+    document.getElementById("answer-hedgehog"),
+    document.getElementById("answer-squirrel"),
+    document.getElementById("answer-hamster"),
+    document.getElementById("answer-beaver")
+];
+
+// init
+let conclusionFinal = document.getElementById("arguments")
+let nbclick = 0;
+
+//ma function
+function conclusion() {
+
+    console.log("click")
+
+    elementHtml.forEach((question) => {
+        question.addEventListener("click", () => {
+            nbclick++;
+
+            if (result < numbertabl / 2) {
+                console.log("test1")
+                conclusionFinal.innerText = (balisePseudo.value + " Get out a bit more !");
+            }
+            if (result === numbertabl) {
+                console.log("test2")
+                conclusionFinal.innerText = (balisePseudo.value + " Is that you tarzan ???");
+            }
+            if (result < numbertabl / 3) {
+                console.log("test2")
+                conclusionFinal.innerText = (balisePseudo.value + " Well done you're a certified parisian !");
+            }
+            if (nbclick === numbertabl) {
+                resetButton.style.visibility = "visible";
+            }
+        });
+    });
+}
+conclusion()
+
+
+
+//*------------------- réessayer ---------------*//
+resetButton.addEventListener("click", (event) => {
+    location.reload(event);
+})
